87dd8ef1e1ea2442e5e9779258235f7483ed400b
jackoe <jackwines@mac.com>
2017-07-15 17:29:17 -0500

    bst is now balanced

--- bst.hs
+++ bst.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 import Data.Maybe
 
-data Tree a =  Node (Tree a) (Tree a) a | Empty
+data Color = R | B deriving (Show, Eq)
+
+data Tree a =  Node Color (Tree a) (Tree a) a | Empty
     deriving (Show, Eq)
+
 
 null :: Ord a => Tree a -> Bool
 null = (==) empty
@@ -11,34 +14,47 @@ empty :: Tree a
 empty = Empty
 
 singleton :: Ord a => a -> Tree a
-singleton = Node Empty Empty
+singleton = Node B Empty Empty
 
 member :: forall a. Ord a =>  a -> Tree a -> Bool
 member elem = member'
     where
-
         member' :: Tree a -> Bool
         member' Empty = False
-        member' (Node left right curr)
+        member' (Node _ left right curr)
                 | elem == curr = True
                 | elem < curr = member' left
                 | otherwise   = member' right
 
 
 insert :: forall a. Ord a => a -> Tree a -> Tree a
-insert elem tree = fromMaybe tree . insert' $ tree
+insert elem tree = fromMaybe tree . insert' . turnBlack $ tree
     where
 
         insert' :: Tree a -> Maybe (Tree a)
-        insert' Empty = Just $ Node Empty Empty elem
-        insert' (Node left right curr)
-          | elem < curr = fmap (\left'  -> Node left' right curr)
-                        . insert'
-                        $ left
-          | elem > curr = fmap (\right' -> Node left right' curr)
-                        . insert'
-                        $ right
-          | otherwise = Nothing
+        insert' Empty = Just $ Node R Empty Empty elem
+        insert' (Node col left right curr)
+          | elem < curr = recurse (\left'  -> Node col left' right curr) left
+          | elem > curr = recurse (\right' -> Node col left right' curr) right
+          | otherwise   = Nothing
+
+        recurse :: (Tree a -> Tree a) -> Tree a -> Maybe (Tree a)
+        recurse makeNode below = fmap (balance . makeNode) . insert' $ below
+
+balance :: Tree a -> Tree a
+balance (Node B (Node R (Node R a b x) c y) d z) = (Node R (Node B a b x) (Node B c d z) y)
+balance (Node B a (Node R b (Node R c d x) y) z) = (Node R (Node B a b x) (Node B c d z) y)
+balance (Node B (Node R a (Node R b c x) y) d z) = (Node R (Node B a b x) (Node B c d z) y)
+balance (Node B a (Node R (Node R b c x) d y) z) = (Node R (Node B a b x) (Node B c d z) y)
+balance a = a
+
+turnBlack :: Tree a -> Tree a
+turnBlack (Node _ left right elem) = Node B left right elem
+turnBlack Empty = Empty
+
+toList :: Tree a -> [a]
+toList Empty = []
+toList (Node _ left right elem) = (toList left) ++ elem : (toList right)
 
 fromList :: (Foldable t, Ord a) => t a -> Tree a
 fromList = foldl (flip insert) empty
